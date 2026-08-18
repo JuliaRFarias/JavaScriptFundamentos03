8586186dae64ca087efafeab298c45e056672d93
JÚLIA ROCHA DE FARIAS <26171617@edu119.local>
2026-08-18 11:48:12 -0300

    Salvando alterações

---
+++ 01-funcoes/07-arrow-function.js
@@ -0,0 +1,14 @@
+function somarTradicional(a, b){
+    return a+b;
+}
+
+const somarAnonima = function (a, b) {
+    return a + b;
+}
+
+const somarArrowFunction = (a,b) =>{
+    return a + b;
+}
+console.log(somarTadicional(10,5));
+console.log(somarAnonima(10,5));
+console.log(somarArrowFunction(10,5));
